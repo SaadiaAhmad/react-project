660815b6bdbd2e8e7c9c308f3b99645381981f7a
SAHB <s75ahmad@uwaterloo.ca>
2024-02-22 15:36:34 -0500

    very basic index.js file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
+// 1. Import the React and ReactDom Libraries
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// 2. Get a reference to the div with Id root
+const el = document.getElementById('root');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 3. Tell React to take control of that element and create a root
+const root = ReactDOM.createRoot(el);
+
+// 4. Create a component
+function App() {
+  let message = 'Bye there!';
+  if(Math.random() > 0.5) message = 'Hello there';
+
+  return (
+    <div>
+      <h1>{message} at {new Date().toLocaleTimeString()}</h1>
+      <input type="number" min={3} placeholder="hi there" />
+      <textarea autoFocus={true}></textarea>
+    </div>
+  );
+}
+
+// 5. Show the component on the screen
+root.render(<App></App>);
