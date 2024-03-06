5730619dbe2902970dd6eeddeed67e52ee746e0e
SAHB <s75ahmad@uwaterloo.ca>
2024-03-06 14:44:24 -0500

    display random image for each book

--- src/components/ReadingList/BookItem/BookItem.js
+++ src/components/ReadingList/BookItem/BookItem.js
@@ -21,13 +21,16 @@ function BookItem(props) {
     let itemContent = <h3>{props.book.title}</h3>;
     if(showEdit) itemContent = <BookEdit titleValue={props.book.title} onEditSave={editbyId}></BookEdit>;
 
+    const imageSrc = `https://picsum.photos/seed/${props.book.id}/200/250`;
+
     return(
         <div className="book-item">
             <div className="actions">
                 <button className="edit" onClick={toggleEdit}></button>
                 <button className="delete" onClick={deleteBookById}></button>
             </div>
-            <div>{itemContent}</div>
+            <img alt="books" src={imageSrc}></img>
+            <div className="content">{itemContent}</div>
         </div>
     )
 }
