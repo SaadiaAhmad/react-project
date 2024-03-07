2a3dc051f22b6dc0c43732f8d3bb05bb24311853
SAHB <s75ahmad@uwaterloo.ca>
2024-03-07 13:06:02 -0500

    use edit and delete APIs

--- src/components/ReadingList/ReadingList.js
+++ src/components/ReadingList/ReadingList.js
@@ -22,15 +22,17 @@ function ReadingList() {
         setBooks(updatedBooks);
     }
 
-    const editBookById = (id, newTitle) => {
+    const editBookById = async (id, newTitle) => {
+        const response = await axios.put(`http://localhost:3001/books/${id}`, {title: newTitle});
         const updatedBooks = books.map((book) => {
-            if(book.id === id) return {...book, title: newTitle}
+            if(book.id === id) return response.data;
             return book;
         });
         setBooks(updatedBooks);
     }
 
-    const deleteBookById = (id) => {
+    const deleteBookById = async (id) => {
+        await axios.delete(`http://localhost:3001/books/${id}`);      
         const updatedBooks = books.filter((book) => book.id !== id);
         setBooks(updatedBooks);
     }
