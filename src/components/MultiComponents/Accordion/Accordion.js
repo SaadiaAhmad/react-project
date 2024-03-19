dd846363050990bf8bc531f14127360f0edae521
Saadia Ahmad <s75ahmad@uwaterloo.ca>
2024-03-19 14:31:22 -0400

    some refactoring of multicomponents

--- src/components/MultiComponents/Accordion/Accordion.js
+++ src/components/MultiComponents/Accordion/Accordion.js
@@ -1,33 +1,17 @@
-import { useState } from "react";
 import { GoChevronDown, GoChevronLeft} from 'react-icons/go';
 
-function Accordion({ items }) {
-    const [ expandedIndex, setExpandedIndex ] = useState(-1);
+function Accordion({ item, isExpanded, onClick }) {
+    const renderedIcon = ( <span className="text-2xl">{isExpanded ? <GoChevronDown></GoChevronDown> : <GoChevronLeft></GoChevronLeft> }</span>);
 
-    const handleClick = (nextIndex) => {
-        setExpandedIndex(nextIndex === expandedIndex ? -1 : nextIndex);
-    }
-
-    const renderedItems = items.map((item, index) => {
-        const isExpanded = index === expandedIndex;
-
-        const renderedIcon = ( <span className="text-2xl">{isExpanded ? <GoChevronDown></GoChevronDown> : <GoChevronLeft></GoChevronLeft> }</span>);
-
-        return (
-            <div key={index}>
-                <h3 className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer" onClick={() => handleClick(index)}>
-                    {item.header}
-                    {renderedIcon}
-                </h3>
-                {isExpanded && <p className="border-b p-5 ">{item.content}</p>}
-            </div>
-        );
-    });
     return (
-        <div className="m-5 border-x border-t rounded">
-            {renderedItems} 
+        <div>
+            <h3 className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer" onClick={onClick}>
+                {item.header}
+                {renderedIcon}
+            </h3>
+            {isExpanded && <p className="border-b p-5 ">{item.content}</p>}
         </div>
-    )
+    );
 }
 
 export default Accordion;
--- src/components/MultiComponents/MultiComponents.js
+++ src/components/MultiComponents/MultiComponents.js
@@ -1,8 +1,7 @@
-import Button from "./Button/Button";
-import { GoBell, GoCloudDownload, GoDatabase, GoGift, GoSmiley, GoZap, GoSquirrel } from 'react-icons/go';
 import './MultiComponents.css';
-import Accordion from "./Accordion/Accordion";
 import Dropdown from "./Dropdown.js/Dropdown";
+import ButtonList from "./ButtonList/ButtonList";
+import AccordionList from './AccordionList/AccordionList';
 
 function MultiComponents() {
     const handleClick = () => {
@@ -49,37 +48,10 @@ function MultiComponents() {
 
             <div className="flex-container">
                 <div className="flex-child">
-                    <Button primary onClick={handleClick} className="mb-5">
-                        <GoBell className="mr-1"></GoBell>
-                        One
-                    </Button>
-                    <Button secondary onClick={handleClick}>
-                        <GoCloudDownload className="mr-1"></GoCloudDownload>
-                        Click me!!
-                    </Button>
-                    <Button success>
-                        <GoDatabase className="mr-1"></GoDatabase>
-                        Buy Now
-                    </Button>
-                    <Button warning>
-                        <GoGift className="mr-1"></GoGift>
-                        Save
-                    </Button>
-                    <Button danger>
-                        <GoZap className="mr-1"></GoZap>
-                        Add to Cart
-                    </Button>
-                    <Button danger rounded>
-                        <GoSmiley className="mr-1"></GoSmiley>
-                        Save to Wishlist
-                    </Button>
-                    <Button success outline>
-                        <GoSquirrel className="mr-1"></GoSquirrel>
-                        Checkout
-                    </Button>
+                    <ButtonList></ButtonList>
                 </div>
                 <div className="flex-child">
-                    <Accordion items={accordionItems}></Accordion>
+                    <AccordionList items={accordionItems}></AccordionList>
                     <Dropdown items={dropdownItems}></Dropdown>
                 </div>
             </div>
