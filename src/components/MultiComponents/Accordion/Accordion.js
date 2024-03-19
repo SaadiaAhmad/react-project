27fb66ec77856c013dd0a1e30e74571409038cdf
Saadia Ahmad <s75ahmad@uwaterloo.ca>
2024-03-19 13:15:04 -0400

    add accordian component

---
+++ src/components/MultiComponents/Accordion/Accordion.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { GoChevronDown, GoChevronLeft} from 'react-icons/go';
+
+function Accordion({ items }) {
+    const [ expandedIndex, setExpandedIndex ] = useState(-1);
+
+    const handleClick = (nextIndex) => {
+        setExpandedIndex(nextIndex === expandedIndex ? -1 : nextIndex);
+    }
+
+    const renderedItems = items.map((item, index) => {
+        const isExpanded = index === expandedIndex;
+
+        const renderedIcon = ( <span className="text-2xl">{isExpanded ? <GoChevronDown></GoChevronDown> : <GoChevronLeft></GoChevronLeft> }</span>);
+
+        return (
+            <div key={index}>
+                <h3 className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer" onClick={() => handleClick(index)}>
+                    {item.header}
+                    {renderedIcon}
+                </h3>
+                {isExpanded && <p className="border-b p-5 ">{item.content}</p>}
+            </div>
+        );
+    });
+    return (
+        <div className="m-5 border-x border-t rounded">
+            {renderedItems} 
+        </div>
+    )
+}
+
+export default Accordion;
--- src/components/MultiComponents/MultiComponents.js
+++ src/components/MultiComponents/MultiComponents.js
@@ -1,9 +1,27 @@
 import Button from "./Button/Button";
 import { GoBell, GoCloudDownload, GoDatabase, GoGift, GoSmiley, GoZap, GoSquirrel } from 'react-icons/go';
+import './MultiComponents.css';
+import Accordion from "./Accordion/Accordion";
+
 function MultiComponents() {
     const handleClick = () => {
         console.log('Button clicked')
     };
+
+    const accordionItems = [
+        {
+            header: 'What day is today?',
+            content: 'It is only Monday'
+        },
+        {
+            header: 'Is it Friday yet?',
+            content: 'Yes, it is'
+        },
+        {
+            header: 'Weekend is here?',
+            content: 'Yayyy'
+        }
+    ]
 
     return (
         <div>
@@ -13,34 +31,43 @@ function MultiComponents() {
                 </div>
             </section>
 
-            <Button primary onClick={handleClick} className="mb-5">
-                <GoBell className="mr-1"></GoBell>
-                One
-            </Button>
-            <Button secondary onClick={handleClick}>
-                <GoCloudDownload className="mr-1"></GoCloudDownload>
-                Click me!!
-            </Button>
-            <Button success>
-                <GoDatabase className="mr-1"></GoDatabase>
-                Buy Now
-            </Button>
-            <Button warning>
-                <GoGift className="mr-1"></GoGift>
-                Save
-            </Button>
-            <Button danger>
-                <GoZap className="mr-1"></GoZap>
-                Add to Cart
-            </Button>
-            <Button danger rounded>
-                <GoSmiley className="mr-1"></GoSmiley>
-                Save to Wishlist
-            </Button>
-            <Button success outline>
-                <GoSquirrel className="mr-1"></GoSquirrel>
-                Checkout
-            </Button>
+            <div className="flex-container">
+                <div className="flex-child">
+                    <Button primary onClick={handleClick} className="mb-5">
+                        <GoBell className="mr-1"></GoBell>
+                        One
+                    </Button>
+                    <Button secondary onClick={handleClick}>
+                        <GoCloudDownload className="mr-1"></GoCloudDownload>
+                        Click me!!
+                    </Button>
+                    <Button success>
+                        <GoDatabase className="mr-1"></GoDatabase>
+                        Buy Now
+                    </Button>
+                    <Button warning>
+                        <GoGift className="mr-1"></GoGift>
+                        Save
+                    </Button>
+                    <Button danger>
+                        <GoZap className="mr-1"></GoZap>
+                        Add to Cart
+                    </Button>
+                    <Button danger rounded>
+                        <GoSmiley className="mr-1"></GoSmiley>
+                        Save to Wishlist
+                    </Button>
+                    <Button success outline>
+                        <GoSquirrel className="mr-1"></GoSquirrel>
+                        Checkout
+                    </Button>
+                </div>
+                <div className="flex-child">
+                    <Accordion items={accordionItems}></Accordion>
+                </div>
+            </div>
+
+
         </div>
     )
 }
